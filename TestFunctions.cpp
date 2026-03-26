6372770ac6f7b0a379a602c9a81e7ba4ed35ad4b
FlawedApostle <smafearnley@gmail.com>
2026-03-26 19:09:23 -0400

    Debbugging:: ErrorChecking for Shader_2

--- TestFunctions.cpp
+++ TestFunctions.cpp
@@ -8,6 +8,9 @@
 
 TestFunctions::~TestFunctions()
 {
+	if (vShader == true) { glDeleteShader(vShader); }
+	if (fShader == true) { glDeleteShader(fShader); }
+	
 }
 
 void TestFunctions::Render1(double time)
@@ -19,6 +22,10 @@ void TestFunctions::Render1(double time)
 GLuint TestFunctions::Render2()
 {
 
+	GLint vertCompiled;
+	GLint fragCompiled;
+	GLint linked;
+
 	const char* vShaderSource =
 		"#version 430	 \n"
 		"void main(void) \n"
@@ -28,22 +35,50 @@ GLuint TestFunctions::Render2()
 		"#version 430	 \n"
 		"out vec4 color; \n"
 		"void main(void) \n"
-		"{color = vec4(0.0,0.0,1.0,1.0);}";
+		"{color = vec4(0.0,0.0,1.0,1.0);}";								// {	if (gl_FragCoord.x < 295) color = vec4(1.0, 0.0, 0.0, 1.0); else color = vec4(0.0, 0.0, 1.0, 1.0);	} 
 
 	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
 	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
 
+
+	// ---- Gather and read the shader txt files
 	glShaderSource(vShader, 1, &vShaderSource, NULL);
 	glShaderSource(fShader, 1, &fShaderSource, NULL);
-	glCompileShader(vShader);
-	// debug
 	
+	// ---- Compile -- VERT
+	glCompileShader(vShader);
+	ErrorChecking::checkOpenGLError();									// debug
+	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
+	if (vertCompiled != 1) {
+		std::cout << "vertex compilation failed" << std::endl;
+		ErrorChecking::printShaderLog(vShader);
+	}
+	// ---- Compile -- FRAG
 	glCompileShader(fShader);
+	ErrorChecking::checkOpenGLError();									// debug
+	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
+	if (fragCompiled != 1) {
+		std::cout << "vertex compilation failed" << std::endl;
+		ErrorChecking::printShaderLog(fShader);
+	}
 
-	GLuint vfProgram = glCreateProgram();
+
+
+	// ---- Attach The Shader
 	glAttachShader(vfProgram, vShader);
 	glAttachShader(vfProgram, fShader);
+	
+	// ---- Link the program
 	glLinkProgram(vfProgram);
+	ErrorChecking::checkOpenGLError();									// debug
+	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
+	if (linked != 1) {
+		std::cout << "linking failed" << std::endl;
+		ErrorChecking::printProgramLog(vfProgram);
+	}
+
+	GLuint vfProgram = glCreateProgram();
+	
 
 	return vfProgram;
 
